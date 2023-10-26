20b67459b9e944d90c54a3213d47d204bd60fa43
Chris Delisa <chrisdelisa@gmail.com>
2023-10-26 21:00:00 +0000

    UserRegistration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,18 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.exception.BadRequestException;
+import com.example.exception.RequestConflictException;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +20,29 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+
+@RestController
+@RequestMapping
 public class SocialMediaController {
 
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private AccountService accountService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.OK)
+    public Account registerUser (@RequestBody Account account) throws BadRequestException, RequestConflictException{
+
+        if(accountService.findByUsername(account.getUsername()) != null){
+            throw new RequestConflictException();
+        }
+        if(account.getUsername().equals("") ||
+            account.getPassword().length() < 4)
+            {
+                throw new BadRequestException();
+            }
+        return this.accountService.saveAccount(account);
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,29 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+
+import com.example.entity.Account;
+import com.example.exception.BadRequestException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account saveAccount(Account account){
+        return accountRepository.save(account);
+    }
+    public Account findByUsername(String username){
+        var accountOptional = accountRepository.findByUsername(username);
+        if(!accountOptional.isPresent()){
+            return null;
+        }
+        return accountOptional.get();
+    }
 }
